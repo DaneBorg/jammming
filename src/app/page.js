565980bef1c923785d3a9e977385919fff161282
Aluxandr <aluxandr.haakon@gmail.com>
2024-07-03 20:30:24 +0100

    Initial commit just after creation and one touch.

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <p>End to end just a simple repo!</p>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
